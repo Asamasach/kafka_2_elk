72b944d30ade99318b2990aba0c9eb17558e878e
Mehdi Sadat Hosseini <31350077+Asamasach@users.noreply.github.com>
2020-03-23 21:34:50 +0430

    make the code clean

--- python_test_producer.py
+++ python_test_producer.py
@@ -1,35 +1,22 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer #for producing data
 from kafka.errors import KafkaError
-import json 
-from faker import Faker
-import random
+import json #to change dict to json
+from faker import Faker #to create random arbitary names
+import random #to create random age
 from random import randint
-import msgpack
+
 import logging
-#from bson import json_util
 
-#producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
-# Asynchronous by default
 fake = Faker('en_US')
 
-
-producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii'))
-#producer.send('asamasach', {'key': 'value'})
-#print("json_sent")
-
-#producer = KafkaProducer(value_serializer=msgpack.dumps)
+producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii')) #set kafkaProducer for json
 for _ in range(10):
     my_dict = {  'first_name': fake.first_name(), 'last_name': fake.last_name(), 'age': randint(0, 100)   } 
     print(str(my_dict))
     print("\n")
-#    print(bytes(my_dict, 'utf-8'))
-#    my_dict_encoded=str(my_dict)
-    future = producer.send('asamasach_3', my_dict)
-#    future = producer.send('asamasach', json.dumps( my_dict, default=json_util).encode('utf-8'))
-# Block for 'synchronous' sends
+    future = producer.send('asamasach_3', my_dict) #send json data to kafka topic named 'asamasach_3'
 try:
-    record_metadata = future.get(timeout=10)
+    record_metadata = future.get(timeout=10) #to chech metadata of sending
 except KafkaError:
     # Decide what to do if produce request failed...
     log.exception()
